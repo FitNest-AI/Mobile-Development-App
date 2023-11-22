3ad632ad901e57e6eb8d3e9969fe0ebb027063d6
rfqagst <agustiarifqi@gmail.com>
2023-11-22 15:45:21 +0700

    Update MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,12 @@ android {
             )
         }
     }
+
+    buildFeatures{
+        //enable view binding
+        viewBinding =  true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +43,16 @@ android {
 }
 
 dependencies {
+
+
+    // CameraX core library using camera2 implementation
+    implementation ("androidx.camera:camera-camera2:1.3.0")
+
+    // CameraX Lifecycle Library
+    implementation ("androidx.camera:camera-lifecycle:1.3.0")
+
+    // CameraX View class
+    implementation ("androidx.camera:camera-view:1.4.0-alpha02")
 
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
