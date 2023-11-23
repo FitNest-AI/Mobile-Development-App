49cfce44dd412dca076b88f67ad80014b216b7d1
SyahrulCM <beatcor25.ori@gmail.com>
2023-11-24 05:31:11 +0700

    Initial Commit

--- app/src/main/java/com/example/fitnestapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/fitnestapp/ui/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.example.fitnestapp.ui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnestapp.R
 import com.example.fitnestapp.databinding.ActivityMainBinding
-import com.example.fitnestapp.ui.createset.CreateSetActivity
-import com.example.fitnestapp.ui.workoutlist.WorkoutListActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -27,6 +19,8 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
+        binding.navView.background = null
+
     }
 
 
