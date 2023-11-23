ecb34e98d6c92ad719b3b84db74c7e00a4e1077a
rfqagst <agustiarifqi@gmail.com>
2023-11-23 13:10:34 +0700

    Add Activity and Folder

---
+++ app/src/main/java/com/example/fitnestapp/ui/set/SetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fitnestapp.ui.set
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnestapp.R
+
+class SetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set)
+    }
+}
