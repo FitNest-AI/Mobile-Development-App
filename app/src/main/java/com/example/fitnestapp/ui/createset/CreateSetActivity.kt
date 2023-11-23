3efc253a24d30267b4dccb668709e61901010db5
rfqagst <agustiarifqi@gmail.com>
2023-11-23 12:58:34 +0700

    Add Activity, update HomeActivity

---
+++ app/src/main/java/com/example/fitnestapp/ui/createset/CreateSetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fitnestapp.ui.createset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnestapp.R
+
+class CreateSetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_set)
+    }
+}
