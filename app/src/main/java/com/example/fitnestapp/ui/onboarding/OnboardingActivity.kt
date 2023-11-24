30f25f14d0d50aa6cbe04a8042a3d4e01a533706
SyahrulCM <beatcor25.ori@gmail.com>
2023-11-24 15:33:28 +0700

    Initial Commit

--- app/src/main/java/com/example/fitnestapp/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/fitnestapp/ui/onboarding/OnboardingActivity.kt
@@ -1,12 +1,25 @@
 package com.example.fitnestapp.ui.onboarding
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fitnestapp.R
+import com.example.fitnestapp.databinding.ActivityMainBinding
+import com.example.fitnestapp.databinding.ActivityOnboardingBinding
+import com.example.fitnestapp.ui.MainActivity
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnGetStarted.setOnClickListener{
+            startActivity(Intent(this@OnboardingActivity, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
