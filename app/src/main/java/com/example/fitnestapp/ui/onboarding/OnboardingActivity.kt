d754b89195ad4b557c6de2d05cd0e0361f391d50
rfqagst <agustiarifqi@gmail.com>
2023-12-05 12:08:08 +0700

    Update Google Auth

--- app/src/main/java/com/example/fitnestapp/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/fitnestapp/ui/onboarding/OnboardingActivity.kt
@@ -1,17 +1,18 @@
 package com.example.fitnestapp.ui.onboarding
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fitnestapp.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnestapp.data.local.UserPreference
 import com.example.fitnestapp.data.local.dataStore
-import com.example.fitnestapp.databinding.ActivityMainBinding
 import com.example.fitnestapp.databinding.ActivityOnboardingBinding
 import com.example.fitnestapp.ui.MainActivity
 import com.example.fitnestapp.ui.createaccount.CreateAccountActivity
-import com.example.fitnestapp.ui.home.HomeFragment
 import com.example.fitnestapp.ui.login.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
@@ -21,6 +22,8 @@ class OnboardingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOnboardingBinding
     private lateinit var userPreference: UserPreference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
@@ -28,23 +31,34 @@ class OnboardingActivity : AppCompatActivity() {
 
         userPreference = UserPreference.getInstance(this.dataStore)
         CoroutineScope(Dispatchers.IO).launch {
-            val user = userPreference.getSession().first()
-            if (user.isLogin){
-                startActivity(Intent(this@OnboardingActivity, MainActivity::class.java))
-                finish()
-            }
+//            val user = userPreference.getSession().first()
+//            if (user.isLogin ){
+//                startActivity(Intent(this@OnboardingActivity, MainActivity::class.java))
+//                finish()
+//            }
+            auth = Firebase.auth
+            val currentUser = auth.currentUser
+            updateUi(currentUser)
         }
 
         Onclick()
     }
 
+    private suspend fun updateUi(currentUser: FirebaseUser?) {
+        val user = userPreference.getSession().first()
+        if (currentUser != null && user.isLogin) {
+            startActivity(Intent(this@OnboardingActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+
     private fun Onclick() {
-        binding.btnGetStarted.setOnClickListener{
+        binding.btnGetStarted.setOnClickListener {
             startActivity(Intent(this@OnboardingActivity, CreateAccountActivity::class.java))
             finish()
         }
 
-        binding.tvBtnSignin.setOnClickListener{
+        binding.tvBtnSignin.setOnClickListener {
             startActivity(Intent(this@OnboardingActivity, LoginActivity::class.java))
             finish()
         }
