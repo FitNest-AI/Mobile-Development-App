59997a5d2233fbbf9f46304472e09e5dde967746
rfqagst <agustiarifqi@gmail.com>
2023-12-07 10:48:17 +0700

    Update Camera Layout

--- app/src/main/java/com/example/fitnestapp/ui/set/SetActivity.kt
+++ app/src/main/java/com/example/fitnestapp/ui/set/SetActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fitnestapp.ui.set
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fitnestapp.databinding.ActivitySetBinding
+import com.example.fitnestapp.ui.camera.CameraActivity
 
 class SetActivity : AppCompatActivity() {
 
@@ -11,5 +13,10 @@ class SetActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySetBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        binding.btnPlay.setOnClickListener {
+            startActivity(Intent(this, CameraActivity::class.java))
+        }
     }
 }
