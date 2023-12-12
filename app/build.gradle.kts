def4217c655a912cf20626666a5da56abef28500
rfqagst <agustiarifqi@gmail.com>
2023-12-12 13:57:43 +0700

    Update Camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ android {
     buildFeatures {
         viewBinding = true
         compose = true
+        mlModelBinding = true
     }
 
     compileOptions {
@@ -83,6 +84,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("org.tensorflow:tensorflow-lite-support:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
+    implementation("org.tensorflow:tensorflow-lite-gpu:2.3.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
--- app/src/main/java/com/example/fitnestapp/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/fitnestapp/ui/camera/CameraActivity.kt
@@ -1,32 +1,43 @@
 package com.example.fitnestapp.ui.camera
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
-import android.hardware.camera2.CameraAccessException
+import android.graphics.*
 import android.hardware.camera2.CameraCaptureSession
-import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
-import android.util.DisplayMetrics
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
-import android.util.Size
 import android.view.Surface
-import android.view.SurfaceHolder
+import android.view.TextureView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.fitnestapp.R
 import com.example.fitnestapp.databinding.ActivityCameraBinding
+import com.example.fitnestapp.ml.AutoModel4
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 
 class CameraActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityCameraBinding
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var model: AutoModel4
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+    lateinit var handler: Handler
+    private lateinit var handlerThread: HandlerThread
+    lateinit var textureView: TextureView
     private lateinit var cameraManager: CameraManager
-    private var cameraDevice: CameraDevice? = null
-    private var captureSession: CameraCaptureSession? = null
-    private var previewRequestBuilder: CaptureRequest.Builder? = null
-
+    private lateinit var binding: ActivityCameraBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraBinding.inflate(layoutInflater)
@@ -40,31 +51,113 @@ class CameraActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 0)
         }
 
-        val surfaceView = binding.surfaceView
-        val surfaceHolder = surfaceView.holder
+
+        imageProcessor =
+            ImageProcessor.Builder().add(ResizeOp(192, 192, ResizeOp.ResizeMethod.BILINEAR)).build()
 
 
+        model = AutoModel4.newInstance(this)
+        imageView = findViewById(R.id.imageView)
+        textureView = findViewById(R.id.textureView)
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
 
-        surfaceHolder.addCallback(object : SurfaceHolder.Callback {
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                openCamera(holder)
+        paint.setColor(Color.YELLOW)
+
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                openCamera()
             }
 
-            override fun surfaceChanged(
-                holder: SurfaceHolder,
-                format: Int,
-                width: Int,
-                height: Int
-            ) {
-                // Reconfigure the camera preview if needed
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+
             }
 
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-                cameraDevice?.close()
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
             }
-        })
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                bitmap = textureView.bitmap!!
+                var tensorImage = TensorImage(DataType.UINT8)
+                tensorImage.load(bitmap)
+                tensorImage = imageProcessor.process(tensorImage)
+
+                val inputFeature0 =
+                    TensorBuffer.createFixedSize(intArrayOf(1, 192, 192, 3), DataType.UINT8)
+                inputFeature0.loadBuffer(tensorImage.buffer)
+
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+
+                val mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+                val h = bitmap.height
+                val w = bitmap.width
+                var x = 0
+
+                Log.d("output__", outputFeature0.size.toString())
+                while (x <= 49) {
+                    if (outputFeature0.get(x + 2) > 0.45) {
+                        canvas.drawCircle(
+                            outputFeature0.get(x + 1) * w,
+                            outputFeature0.get(x) * h,
+                            10f,
+                            paint
+                        )
+                    }
+                    x += 3
+                }
+
+                imageView.setImageBitmap(mutable)
+            }
+        }
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun openCamera() {
+        cameraManager.openCamera(
+            cameraManager.cameraIdList[0],
+            object : CameraDevice.StateCallback() {
+                override fun onOpened(p0: CameraDevice) {
+                    var captureRequest = p0.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                    var surface = Surface(textureView.surfaceTexture)
+                    captureRequest.addTarget(surface)
+                    p0.createCaptureSession(
+                        listOf(surface),
+                        object : CameraCaptureSession.StateCallback() {
+                            override fun onConfigured(p0: CameraCaptureSession) {
+                                p0.setRepeatingRequest(captureRequest.build(), null, null)
+                            }
+
+                            override fun onConfigureFailed(p0: CameraCaptureSession) {
+
+                            }
+                        },
+                        handler
+                    )
+                }
+
+                override fun onDisconnected(p0: CameraDevice) {
+
+                }
+
+                override fun onError(p0: CameraDevice, p1: Int) {
+
+                }
+            },
+            handler
+        )
+    }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -76,7 +169,7 @@ class CameraActivity : AppCompatActivity() {
             PERMISSION_REQUEST_CODE -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d("CameraActivity", "Izin kamera diberikan")
-                    openCamera(binding.surfaceView.holder) // Memperbaiki pemanggilan openCamera
+                    openCamera() // Memperbaiki pemanggilan openCamera
                 } else {
                     Log.d("CameraActivity", "Izin kamera ditolak")
                     // Tampilkan pesan bahwa izin kamera diperlukan
@@ -85,111 +178,6 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
-    private fun openCamera(holder: SurfaceHolder) {
-        try {
-            val cameraId =
-                cameraManager.cameraIdList[1] // Pilih ID kamera, misalnya kamera belakang
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                return
-            }
-            val displayMetrics = DisplayMetrics()
-            windowManager.defaultDisplay.getMetrics(displayMetrics)
-            val screenWidth = displayMetrics.widthPixels
-            val screenHeight = displayMetrics.heightPixels
-
-            val characteristics = cameraManager.getCameraCharacteristics(cameraId)
-            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
-            val supportedSizes = map?.getOutputSizes(SurfaceHolder::class.java)
-            // Di sini, Anda dapat memilih resolusi pratinjau yang sesuai dengan layar Anda
-            val selectedSize = chooseOptimalSize(supportedSizes, screenWidth, screenHeight)
-
-            // Set proporsi aspek pratinjau sesuai dengan ukuran yang dipilih
-            holder.setFixedSize(selectedSize.width, selectedSize.height)
-
-
-            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-                override fun onOpened(camera: CameraDevice) {
-                    cameraDevice = camera
-                    createCameraPreviewSession(camera, holder.surface)
-                }
-
-                override fun onDisconnected(camera: CameraDevice) {
-                    camera.close()
-                }
-
-                override fun onError(camera: CameraDevice, error: Int) {
-                    camera.close()
-                }
-            }, null)
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun chooseOptimalSize(sizes: Array<Size>?, width: Int, height: Int): Size {
-        val desiredRatio = height.toDouble() / width.toDouble()
-
-        var selectedSize: Size? = null
-        var minDiff = Double.MAX_VALUE
-
-        for (size in sizes ?: emptyArray()) {
-            val ratio = size.width.toDouble() / size.height.toDouble()
-            val diff = Math.abs(ratio - desiredRatio)
-            if (diff < minDiff) {
-                selectedSize = size
-                minDiff = diff
-            }
-        }
-
-        return selectedSize ?: sizes?.get(0) ?: Size(
-            640,
-            480
-        ) // Default size jika tidak ada yang cocok
-    }
-
-    private fun createCameraPreviewSession(camera: CameraDevice, surface: Surface) {
-        try {
-            previewRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            previewRequestBuilder?.addTarget(surface)
-
-            camera.createCaptureSession(
-                listOf(surface),
-                object : CameraCaptureSession.StateCallback() {
-                    override fun onConfigured(session: CameraCaptureSession) {
-                        Log.d("CameraActivity", "Sesi pratinjau dikonfigurasi")
-
-                        if (cameraDevice == null) return
-
-                        captureSession = session
-                        previewRequestBuilder?.set(
-                            CaptureRequest.CONTROL_AF_MODE,
-                            CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
-                        )
-                        val previewRequest = previewRequestBuilder?.build()
-                        session.setRepeatingRequest(previewRequest!!, null, null)
-                    }
-
-                    override fun onConfigureFailed(session: CameraCaptureSession) {
-                        Log.e("CameraActivity", "Create capture session failed")
-                    }
-                },
-                null
-            )
-        } catch (e: CameraAccessException) {
-            e.printStackTrace()
-            Log.e("CameraActivity", "Exception saat membuat sesi pratinjau", e)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        captureSession?.close()
-        cameraDevice?.close()
-    }
 
     companion object {
         private const val PERMISSION_REQUEST_CODE = 1
