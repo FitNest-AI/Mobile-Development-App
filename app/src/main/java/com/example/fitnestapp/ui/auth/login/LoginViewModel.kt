8420f2faaad01dd839aaa8bfbb0c907a6a66b336
rfqagst <agustiarifqi@gmail.com>
2023-12-20 15:11:29 +0700

    Add isInsert Logic

--- app/src/main/java/com/example/fitnestapp/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/example/fitnestapp/ui/auth/login/LoginViewModel.kt
@@ -6,14 +6,13 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fitnestapp.data.local.UserModel
-import com.example.fitnestapp.data.remote.response.ErrorResponse
 import com.example.fitnestapp.data.remote.response.ResponseLogin
 import com.example.fitnestapp.data.repo.UserRepo
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
-class LoginViewModel(private val userRepo: UserRepo): ViewModel() {
+class LoginViewModel(private val userRepo: UserRepo) : ViewModel() {
 
     private val _login = MutableLiveData<ResponseLogin>()
     val login: LiveData<ResponseLogin> = _login
@@ -23,26 +22,40 @@ class LoginViewModel(private val userRepo: UserRepo): ViewModel() {
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> = _isLoading
 
-    fun postLogin(email: String, password: String){
+    private val _isVerify: MutableLiveData<Boolean?> = MutableLiveData()
+    val isVerify: MutableLiveData<Boolean?> = _isVerify
+
+    private val _errorMessage = MutableLiveData<String?>()
+    val errorMessage: MutableLiveData<String?> = _errorMessage
+
+
+    fun postLogin(email: String, password: String) {
         viewModelScope.launch {
             try {
                 _isLoading.value = true
-                val response = userRepo.login(email,password)
+                val response = userRepo.login(email, password)
                 _isLoading.value = false
-                if (response.success == true){
+                if (response.success == true) {
                     val token = response.data?.user?.token
-                    Log.d("Login","$token")
+//                    val isVerify = response.data?.user?.verify
+                    isVerify.postValue(response.data?.user?.verify)
+
+                    Log.d("Login", "$token")
                     saveSession(UserModel(email, token ?: "", true, isInsertProfile = false))
                     loginStatus.postValue(true)
-                }else{
+                } else {
                     loginStatus.postValue(false)
                 }
-            }catch (e: HttpException) {
-                _isLoading.postValue(false)
+            } catch (e: HttpException) {
+                _isLoading.value = false
                 val jsonInString = e.response()?.errorBody()?.string()
-                val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
-                val errorMessage = errorBody.message
-                Log.d("Login", "$errorMessage")
+                val errorBody = Gson().fromJson(jsonInString, ResponseLogin::class.java)
+                errorMessage.postValue(errorBody.message)
+                Log.d("LoginError", "$errorMessage")
+            } catch (e: Exception) {
+                _isLoading.value = false
+                errorMessage.postValue("Terjadi kesalahan saat pendaftaran")
+                loginStatus.postValue(false)
             }
         }
     }
